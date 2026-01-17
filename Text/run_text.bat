c2822ca4a96a0585f08c772ecfbd0215cc8fb552
stoneboat <asm650c816@gmail.com>
2026-01-17 12:09:47 +0700

    Add files via upload

---
+++ Text/run_text.bat
@@ -0,0 +1,81 @@
+copy dummy.bin opening_text_vn.bin
+copy dummy.bin opening_text_en.bin
+copy dummy.bin stage2_text_vn.bin
+copy dummy.bin stage2_text_en.bin
+copy dummy.bin stage3_text_vn.bin
+copy dummy.bin stage3_text_en.bin
+copy dummy.bin stage4_text_vn.bin
+copy dummy.bin stage4_text_en.bin
+copy dummy.bin stage5_text_vn.bin
+copy dummy.bin stage5_text_en.bin
+copy dummy.bin stage6_text_vn.bin
+copy dummy.bin stage6_text_en.bin
+copy dummy.bin stage7_text_vn.bin
+copy dummy.bin stage7_text_en.bin
+copy dummy.bin stage8_text_vn.bin
+copy dummy.bin stage8_text_en.bin
+copy dummy.bin ending00_vn.bin
+copy dummy.bin ending01_vn.bin
+copy dummy.bin ending02_vn.bin
+copy dummy.bin ending03_vn.bin
+copy dummy.bin ending04_vn.bin
+copy dummy.bin ending05_vn.bin
+copy dummy.bin ending00_en.bin
+copy dummy.bin ending01_en.bin
+copy dummy.bin ending02_en.bin
+copy dummy.bin ending03_en.bin
+copy dummy.bin ending04_en.bin
+copy dummy.bin ending05_en.bin
+copy dummy.bin theking_vn.bin
+copy dummy.bin theking_en.bin
+copy dummy.bin himitsu_easy_vn.bin
+copy dummy.bin himitsu_normal_vn.bin
+copy dummy.bin himitsu_hard_vn.bin
+copy dummy.bin himitsu_master_vn.bin
+copy dummy.bin himitsu_easy_en.bin
+copy dummy.bin himitsu_normal_en.bin
+copy dummy.bin himitsu_hard_en.bin
+copy dummy.bin himitsu_master_en.bin
+
+
+atlas -d log.txt opening_text_vn.bin opening_text_vn.txt
+atlas -d log.txt opening_text_en.bin opening_text_en.txt
+atlas -d log.txt  stage2_text_vn.bin stage2_text_vn.txt
+atlas -d log.txt  stage2_text_en.bin stage2_text_en.txt
+atlas -d log.txt  stage3_text_vn.bin stage3_text_vn.txt
+atlas -d log.txt  stage3_text_en.bin stage3_text_en.txt
+atlas -d log.txt  stage4_text_vn.bin stage4_text_vn.txt
+atlas -d log.txt  stage4_text_en.bin stage4_text_en.txt
+atlas -d log.txt  stage5_text_vn.bin stage5_text_vn.txt
+atlas -d log.txt  stage5_text_en.bin stage5_text_en.txt
+atlas -d log.txt  stage6_text_vn.bin stage6_text_vn.txt
+atlas -d log.txt  stage6_text_en.bin stage6_text_en.txt
+atlas -d log.txt  stage7_text_vn.bin stage7_text_vn.txt
+atlas -d log.txt  stage7_text_en.bin stage7_text_en.txt
+atlas -d log.txt  stage8_text_vn.bin stage8_text_vn.txt
+atlas -d log.txt  stage8_text_en.bin stage8_text_en.txt
+atlas -d log.txt  ending00_vn.bin ending00_vn.txt
+atlas -d log.txt  ending01_vn.bin ending01_vn.txt
+atlas -d log.txt  ending02_vn.bin ending02_vn.txt
+atlas -d log.txt  ending03_vn.bin ending03_vn.txt
+atlas -d log.txt  ending04_vn.bin ending04_vn.txt
+atlas -d log.txt  ending05_vn.bin ending05_vn.txt
+atlas -d log.txt  ending00_en.bin ending00_en.txt
+atlas -d log.txt  ending01_en.bin ending01_en.txt
+atlas -d log.txt  ending02_en.bin ending02_en.txt
+atlas -d log.txt  ending03_en.bin ending03_en.txt
+atlas -d log.txt  ending04_en.bin ending04_en.txt
+atlas -d log.txt  ending05_en.bin ending05_en.txt
+atlas -d log.txt  theking_vn.bin theking_vn.txt
+atlas -d log.txt  theking_en.bin theking_en.txt
+atlas -d log.txt  himitsu_easy_vn.bin himitsu_easy_vn.txt
+atlas -d log.txt  himitsu_normal_vn.bin himitsu_normal_vn.txt
+atlas -d log.txt  himitsu_hard_vn.bin himitsu_hard_vn.txt
+atlas -d log.txt  himitsu_master_vn.bin himitsu_master_vn.txt
+atlas -d log.txt  himitsu_easy_en.bin himitsu_easy_en.txt
+atlas -d log.txt  himitsu_normal_en.bin himitsu_normal_en.txt
+atlas -d log.txt  himitsu_hard_en.bin himitsu_hard_en.txt
+atlas -d log.txt  himitsu_master_en.bin himitsu_master_en.txt
+
+
+pause
